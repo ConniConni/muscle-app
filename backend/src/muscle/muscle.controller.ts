3f47986d0fc86f9fe94e0333fbcd52c3bec6c4a2
ConniConni <nmtgktmo@gmail.com>
2025-04-11 23:28:19 +0900

    全取得API_サービス追加

--- backend/src/muscle/muscle.controller.ts
+++ backend/src/muscle/muscle.controller.ts
@@ -1,10 +1,15 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 import { MuscleService } from './muscle.service';
 import { CreateMuscleDto } from './dto/create-muscle.dto';
 
 @Controller('muscle')
 export class MuscleController {
   constructor(private readonly muscleService: MuscleService) {}
+
+  @Get()
+  async findAll() {
+    return await this.muscleService.findAll();
+  }
 
   @Post()
   async create(@Body() createMuscleDto: CreateMuscleDto) {
