94e81ad1fa64373d7f9e0958df905f5d6deb415d
ConniConni <nmtgktmo@gmail.com>
2025-04-29 17:31:50 +0900

    push取り消し：Dataコンポーネント削除

--- frontend/app/pages/Top.tsx
+++ frontend/app/pages/Top.tsx
@@ -1,6 +1,4 @@
 import { useState } from "react";
-import List from "./components/List";
-
 type Muscle = {
   id: number;
   category_id: number;
@@ -15,6 +13,7 @@ export function Top() {
     const response = await fetch("http://localhost:3000/muscle/");
     const result = await response.json();
     setMuscle(result);
+
     console.log(result, "test");
   };
 
@@ -22,7 +21,31 @@ export function Top() {
     <div className="top">
       <h1>筋トレ実績</h1>
       <button onClick={getMuscle}>一覧取得</button>
-      <List muscle={muscle} />
+      <div>
+        {muscle.map((c) => {
+          const date = new Date(c.date);
+          const formattedDate = date.toLocaleDateString("ja-JP");
+
+          return (
+            <table key={c.id}>
+              <thead>
+                <tr>
+                  <th className="muscle-header">種目ID</th>
+                  <th className="muscle-header">実施日</th>
+                  <th className="muscle-header">回数</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th className="muscle-cell">{c.category_id}</th>
+                  <th className="muscle-cell">{formattedDate}</th>
+                  <th className="muscle-cell">{c.count}</th>
+                </tr>
+              </tbody>
+            </table>
+          );
+        })}
+      </div>
     </div>
   );
 }
