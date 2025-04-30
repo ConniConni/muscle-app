8ecd59f2d8e29cf63c00df43d101506968bbf8d9
ConniConni <nmtgktmo@gmail.com>
2025-04-30 14:40:51 +0900

    topコンポーネントを分割しResultコンポーネントに一覧表示処理を記載

--- frontend/app/pages/Top.tsx
+++ frontend/app/pages/Top.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import Result from "./components/Result";
+
 type Muscle = {
   id: number;
   name: string;
@@ -21,31 +23,7 @@ export function Top() {
     <div className="top">
       <h1>筋トレ実績</h1>
       <button onClick={getMuscle}>一覧取得</button>
-      <div>
-        <table>
-          <thead>
-            <tr>
-              <th className="muscle-header">種目名</th>
-              <th className="muscle-header">実施日</th>
-              <th className="muscle-header">回数</th>
-            </tr>
-          </thead>
-          <tbody>
-            {muscle.map((c) => {
-              const date = new Date(c.date);
-              const formattedDate = date.toLocaleDateString("ja-JP");
-
-              return (
-                <tr key={c.id}>
-                  <th className="muscle-cell">{c.name}</th>
-                  <th className="muscle-cell">{formattedDate}</th>
-                  <th className="muscle-cell">{c.count}</th>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+      <Result muscle={muscle} />
     </div>
   );
 }
--- frontend/app/pages/components/Result.tsx
+++ frontend/app/pages/components/Result.tsx
@@ -1,4 +1,37 @@
-const Result = () => {
-  return;
+type Muscle = {
+  id: number;
+  name: string;
+  date: Date;
+  count: number;
+};
+
+const Result = ({ muscle }: { muscle: Muscle[] }) => {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th className="muscle-header">種目名</th>
+            <th className="muscle-header">実施日</th>
+            <th className="muscle-header">回数</th>
+          </tr>
+        </thead>
+        <tbody>
+          {muscle.map((c) => {
+            const date = new Date(c.date);
+            const formattedDate = date.toLocaleDateString("ja-JP");
+
+            return (
+              <tr key={c.id}>
+                <th className="muscle-cell">{c.name}</th>
+                <th className="muscle-cell">{formattedDate}</th>
+                <th className="muscle-cell">{c.count}</th>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 };
 export default Result;
