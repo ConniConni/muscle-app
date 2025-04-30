04666345955ee9815092e73a2ed9ce06131761a1
ConniConni <nmtgktmo@gmail.com>
2025-04-30 14:59:06 +0900

    top.tsxでMuscleTypeを使用する形に変更

--- frontend/app/pages/Top.tsx
+++ frontend/app/pages/Top.tsx
@@ -1,15 +1,9 @@
 import { useState } from "react";
 import Result from "./components/Result";
-
-type Muscle = {
-  id: number;
-  name: string;
-  date: Date;
-  count: number;
-};
+import type { MuscleType } from "~/type/type";
 
 export function Top() {
-  const [muscle, setMuscle] = useState<Muscle[]>([]);
+  const [muscle, setMuscle] = useState<MuscleType[]>([]);
 
   const getMuscle = async () => {
     const response = await fetch("http://localhost:3000/muscle/");
