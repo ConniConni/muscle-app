babc6973844ca0d868dad22855f5af16070c78e7
ConniConni <nmtgktmo@gmail.com>
2025-05-06 14:30:43 +0900

    propsの型定義追加

--- frontend/app/pages/components/CategorySelectionPulldown.tsx
+++ frontend/app/pages/components/CategorySelectionPulldown.tsx
@@ -2,10 +2,18 @@
 課題1. setFilerValの型をanyから修正する
 課題2. プルダウンをハードコーディングからmst_muscle_categoryから取得できるようにする
 */
+import React from "react";
+type CategorySelectionPulldownProps = {
+  setFilterVal: React.Dispatch<React.SetStateAction<number>>; // setFilterVal の型を定義
+  test: string;
+};
 
-const CategorySelectionPulldown = ({ setFilterVal }: any) => {
+const CategorySelectionPulldown = ({
+  setFilterVal,
+  test,
+}: CategorySelectionPulldownProps) => {
   return (
-    <select name="category_id" onChange={(e) => setFilterVal(e.target.value)}>
+    <select name="category_id" onChange={(e) => setFilterVal(+e.target.value)}>
       <option value="1">腹筋</option>
       <option value="2">腕立て</option>
       <option value="3">背筋</option>
