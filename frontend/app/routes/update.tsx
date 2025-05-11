1f177bc420f067cb66aca0cd97902cee787cbba4
ConniConni <nmtgktmo@gmail.com>
2025-05-11 14:56:03 +0900

    updateコンポーネントのmetaでtitleコンポーネントを利用する形に変更

--- frontend/app/routes/update.tsx
+++ frontend/app/routes/update.tsx
@@ -1,11 +1,10 @@
+import Title from "~/pages/components/title";
 import type { Route } from "../+types/root";
 import UpdatePage from "~/pages/components/UpdatePage";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "筋トレ管理アプリ" },
-    { name: "description", content: "筋トレ登録" },
-  ];
+  const titleProps = Title();
+  return [titleProps];
 }
 
 export default function Update() {
