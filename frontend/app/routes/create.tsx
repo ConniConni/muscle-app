be3a55b9550195b4c28353c8d2a044fb78802244
ConniConni <nmtgktmo@gmail.com>
2025-05-11 16:12:44 +0900

    createコンポーネントでInputFormコンポーネントを使う形に修正

--- frontend/app/routes/create.tsx
+++ frontend/app/routes/create.tsx
@@ -2,6 +2,7 @@ import Button from "~/pages/components/Button";
 import type { Route } from "../+types/root";
 import { useNavigate } from "react-router";
 import Title from "~/utils/Title";
+import InputForm from "~/pages/components/InputForm";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -54,24 +55,7 @@ export default function Create() {
 
   return (
     <div>
-      <h1>新規登録ぺージ</h1>
-      {/* SPAの時はreactはonSubmitを使用する */}
-      <form action={createTraining}>
-        <div>
-          <select name="category_id">
-            <option value="1">腹筋</option>
-            <option value="2">腕立て</option>
-            <option value="3">背筋</option>
-          </select>
-        </div>
-        <div>
-          <input type="date" name="date" />
-        </div>
-        <div>
-          <input type="number" name="count" placeholder="10" />
-        </div>
-        <button type="submit">登録</button>
-      </form>
+      <InputForm onClick={createTraining} actionName="登録" />
       <Button onClick={backTopPage} buttonName="戻る" />
     </div>
   );
