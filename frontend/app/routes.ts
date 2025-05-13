5f6355efe3c59860a90d42ab0aaf8204bfc8838b
ConniConni <nmtgktmo@gmail.com>
2025-05-14 08:56:22 +0900

    マスタ管理画面のルーティングを設定

--- frontend/app/routes.ts
+++ frontend/app/routes.ts
@@ -4,4 +4,5 @@ export default [
   route("/", "routes/home.tsx"),
   route("create", "routes/create.tsx"),
   route("update/:id", "routes/update.tsx"),
+  route("manage-mst-muscle-category/", "routes/manageMstMuscleCategory.tsx"),
 ] satisfies RouteConfig;
