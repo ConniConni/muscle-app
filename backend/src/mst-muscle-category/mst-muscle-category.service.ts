c568e3c9a46db9edb38748a5db971756a795eb52
ConniConni <nmtgktmo@gmail.com>
2025-05-13 20:49:32 +0900

    マスタ登録処理を実装

--- backend/src/mst-muscle-category/mst-muscle-category.service.ts
+++ backend/src/mst-muscle-category/mst-muscle-category.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma.service';
+import { CreateMstMuscleCategoryDto } from './dto/create-mst-muscle-category.dto';
 
 @Injectable()
 export class MstMuscleCategoryService {
@@ -14,5 +15,13 @@ export class MstMuscleCategoryService {
       ORDER BY mmc.id ASC;
       `;
     return result;
+  }
+
+  async create(mstMuscleCategoryDto: CreateMstMuscleCategoryDto) {
+    const result = await this.prisma.$executeRaw`
+    INSERT INTO mst_muscle_category (name) VALUES (
+    ${mstMuscleCategoryDto.name}
+    )
+    `;
   }
 }
