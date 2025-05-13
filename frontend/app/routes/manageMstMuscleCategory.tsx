241cb6da2b4b1df6f2e0c371f3cbe0d1e5e4891b
ConniConni <nmtgktmo@gmail.com>
2025-05-14 08:50:17 +0900

    マスタ管理画面を作成

---
+++ frontend/app/routes/manageMstMuscleCategory.tsx
@@ -0,0 +1,16 @@
+import type { Route } from "../+types/root";
+import Title from "~/utils/Title";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    {
+      title: Title.title,
+      description: Title.description,
+      content: Title.content,
+    },
+  ];
+}
+
+export default function Update() {
+  return <h1>テスト</h1>;
+}
