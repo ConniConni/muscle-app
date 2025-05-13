99b119049ccb292e93793886a7cf07c13fa64a9a
ConniConni <nmtgktmo@gmail.com>
2025-05-13 15:39:55 +0900

    不要な記述を削除

--- frontend/app/pages/components/UpdatePage.tsx
+++ frontend/app/pages/components/UpdatePage.tsx
@@ -5,14 +5,13 @@ import { useNavigate, useParams } from "react-router";
 const UpdatePage = () => {
   // URL から id を取得
   const { id } = useParams<{ id: string }>();
-  const idNumber = Number(id);
 
   const updateTraining = async (formData: FormData) => {
     const categoryId = formData.get("category_id");
     const date = formData.get("date");
     const count = formData.get("count");
     if (categoryId && date && count) {
-      await fetch(`http://localhost:3000/muscle/${idNumber}`, {
+      await fetch(`http://localhost:3000/muscle/${id}`, {
         method: "PATCH",
         headers: {
           "Content-Type": "application/json",
