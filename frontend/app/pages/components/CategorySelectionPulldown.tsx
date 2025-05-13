cf60714228e972c33dc93f38f3c2b778913bfdef
ConniConni <nmtgktmo@gmail.com>
2025-05-13 19:29:37 +0900

    マスタの全取得結果を保持するstateを定義する

--- frontend/app/pages/components/CategorySelectionPulldown.tsx
+++ frontend/app/pages/components/CategorySelectionPulldown.tsx
@@ -2,14 +2,21 @@
 課題1. setFilerValの型をanyから修正する
 課題2. プルダウンをハードコーディングからmst_muscle_categoryから取得できるようにする
 */
-import React from "react";
+import React, { useState } from "react";
 type CategorySelectionPulldownProps = {
   setFilterVal: React.Dispatch<React.SetStateAction<number>>;
+};
+
+type MstMuscleCategory = {
+  id: number;
+  name: string;
 };
 
 const CategorySelectionPulldown = ({
   setFilterVal,
 }: CategorySelectionPulldownProps) => {
+  const [trainingName, setTrainingName] = useState<MstMuscleCategory[]>([]);
+
   return (
     <select name="category_id" onChange={(e) => setFilterVal(+e.target.value)}>
       <option value="1">腹筋</option>
