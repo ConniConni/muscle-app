2f59ed693fa4ea67f77510d74d8211afdc15eb5a
ConniConni <nmtgktmo@gmail.com>
2025-05-14 15:36:17 +0900

    Createの処理ををコンポーネントに分割

---
+++ frontend/app/pages/components/postTrainingDataPage.tsx
@@ -0,0 +1,51 @@
+import { useNavigate } from "react-router";
+import InputForm from "./InputForm";
+import Button from "./Button";
+
+const PostTrainingDataPage = () => {
+  const createTraining = async (formData: FormData) => {
+    const categoryId = formData.get("category_id");
+    const date = formData.get("date");
+    const count = formData.get("count");
+    if (categoryId && date && count) {
+      await fetch("http://localhost:3000/muscle/", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          category_id: +categoryId!,
+          date: date,
+          count: +count!,
+        }),
+      });
+      alert("登録が完了しました。");
+      backTopPage();
+    } else {
+      const alertMessage: string[] = [];
+      if (!categoryId) {
+        alertMessage.push("トレーニングを選択してください");
+      }
+      if (!date) {
+        alertMessage.push("実施日を選択してください");
+      }
+      if (!count) {
+        alertMessage.push("回数を入力してください");
+      }
+      alert(alertMessage.join("\n"));
+    }
+  };
+
+  const navigate = useNavigate();
+  const backTopPage = () => {
+    navigate("/");
+  };
+
+  return (
+    <div>
+      <InputForm onClick={createTraining} actionName="登録" />
+      <Button onClick={backTopPage} buttonName="戻る" />
+    </div>
+  );
+};
+export default PostTrainingDataPage;
