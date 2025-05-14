c24faf41692f85ed90278dc7a81c96308f0fdb30
ConniConni <nmtgktmo@gmail.com>
2025-05-14 11:00:11 +0900

    マスタ取得処理を呼び出し、stateに保持

--- frontend/app/routes/manageMstMuscleCategory.tsx
+++ frontend/app/routes/manageMstMuscleCategory.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import type { Route } from "../+types/root";
 import Title from "~/utils/Title";
 import type { TrainingCategory } from "~/type/training_category_type";
@@ -17,5 +17,15 @@ export default function Update() {
   const [trainingCategory, setTrainingCategory] = useState<TrainingCategory[]>(
     []
   );
+  const getMstMuscleCategory = async () => {
+    const response = await fetch("http://localhost:3000/mst-muscle-category");
+    const result = await response.json();
+    setTrainingCategory(result);
+    console.log(result);
+  };
+  useEffect(() => {
+    getMstMuscleCategory();
+  }, []);
+
   return <h1>テスト</h1>;
 }
