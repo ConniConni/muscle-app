e117fd92c05246b18a44b9d74c30963bbb51ac6b
ConniConni <nmtgktmo@gmail.com>
2025-05-15 14:42:03 +0900

    登録ページに遷移した際は、個別データ取得処理が入らないように修正

--- frontend/app/pages/components/InputForm.tsx
+++ frontend/app/pages/components/InputForm.tsx
@@ -18,10 +18,12 @@ const InputForm = (props: Props) => {
 
   useEffect(() => {
     (async () => {
-      const response = await fetch(`http://localhost:3000/muscle/id/${id}`);
-      const result = await response.json();
-      console.log("api取得結果:", result);
-      setTrainingData({ ...result, date: new Date(result.date) });
+      if (id) {
+        const response = await fetch(`http://localhost:3000/muscle/id/${id}`);
+        const result = await response.json();
+        console.log("api取得結果:", result);
+        setTrainingData({ ...result, date: new Date(result.date) });
+      }
     })();
   }, []);
   useEffect(() => {
