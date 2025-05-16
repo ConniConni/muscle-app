677bc49044676be7aa2e46b2797dabca3e1cb63d
ConniConni <nmtgktmo@gmail.com>
2025-05-16 16:53:57 +0900

    サイドバーに筋トレ実績画面への戻るボタンを追加

--- frontend/app/pages/components/Sidebar.tsx
+++ frontend/app/pages/components/Sidebar.tsx
@@ -1,4 +1,15 @@
+import { useNavigate } from "react-router";
+import Button from "./Button";
+
 const Sidebar = () => {
-  return <aside className="sidebar"></aside>;
+  const navigate = useNavigate();
+  const backTopPage = () => {
+    navigate("/");
+  };
+  return (
+    <aside className="sidebar">
+      <Button onClick={backTopPage} buttonName="筋トレ実績へ" />
+    </aside>
+  );
 };
 export default Sidebar;
