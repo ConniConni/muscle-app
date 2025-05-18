bc16282e2807fe82fc73db0345581603df10ec5c
ConniConni <nmtgktmo@gmail.com>
2025-05-18 17:54:03 +0900

    buttonコンポーネント内でボタンのホバー時の色と背景色を管理する形に修正

--- frontend/app/pages/components/Button.tsx
+++ frontend/app/pages/components/Button.tsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
+
 type Props = {
   buttonName: string;
   type?: "button" | "submit";
   onClick?: () => void;
   disabled?: boolean;
+  color?: string;
   background?: string;
+  hoverBackground?: string;
+  hoverColor?: string;
 };
 
 const Button = (prop: Props) => {
+  const [isHovered, setIsHovered] = useState<Boolean>(false);
   return (
     <button
       onClick={prop.onClick}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
       type={prop.type || "button"}
       style={{
-        background: prop.background || "lightgrey",
+        color: isHovered ? prop.hoverColor || "white" : prop.color || "black",
+        background: isHovered
+          ? prop.hoverBackground || "darkgray"
+          : prop.background || "lightgrey",
         borderRadius: "10px",
         maxWidth: "250px",
         padding: "5px",
