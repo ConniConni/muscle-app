2b4647d916bba7136acd3cd9544cc540029459a5
ConniConni <nmtgktmo@gmail.com>
2025-05-18 16:40:12 +0900

    Buttonコンポーネント内にCSSスタイルを追加

--- frontend/app/pages/components/Button.tsx
+++ frontend/app/pages/components/Button.tsx
@@ -2,9 +2,22 @@ type Props = {
   onClick: () => void;
   buttonName: string;
   disabled?: boolean;
+  background?: string;
 };
 
 const Button = (prop: Props) => {
-  return <button onClick={prop.onClick}>{prop.buttonName}</button>;
+  return (
+    <button
+      onClick={prop.onClick}
+      style={{
+        background: prop.background || "lightgrey",
+        borderRadius: "10px",
+        maxWidth: "250px",
+        padding: "5px",
+      }}
+    >
+      {prop.buttonName}
+    </button>
+  );
 };
 export default Button;
