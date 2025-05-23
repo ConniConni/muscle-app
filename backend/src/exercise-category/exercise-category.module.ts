bde0162e4d0aca25a8845bd8579b525e8af7a875
ConniConni <nmtgktmo@gmail.com>
2025-05-23 22:45:43 +0900

    種目テーブル名前整理

---
+++ backend/src/exercise-category/exercise-category.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { ExerciseCategoryService } from './exercise-category.service';
+import { PrismaService } from 'src/prisma.service';
+import { ExerciseCategoryController } from './exercise-category.controller';
+
+@Module({
+  controllers: [ExerciseCategoryController],
+  providers: [ExerciseCategoryService, PrismaService],
+})
+export class MstMuscleCategoryModule {}
---
+++ backend/src/exercise-category/exercise-category.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { ExerciseCategoryDto } from './dto/exercise-category.dto';
+
+@Injectable()
+export class ExerciseCategoryService {
+  constructor(private prisma: PrismaService) {}
+
+  async findAll() {
+    const result = await this.prisma.$queryRaw`
+      SELECT
+        ec.id,
+        ec.name
+      FROM exercise_categories as ec
+      ORDER BY ec.id ASC;
+      `;
+    return result;
+  }
+
+  async create(exerciseCategoryDto: ExerciseCategoryDto) {
+    const result = await this.prisma.$executeRaw`
+    INSERT INTO exercise_categories (name) VALUES (
+    ${exerciseCategoryDto.name}
+    )
+    `;
+  }
+}
