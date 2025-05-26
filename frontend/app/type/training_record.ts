7d766b2d1805d0cc771cecc9a0027bf573cdf752
ConniConni <nmtgktmo@gmail.com>
2025-05-26 10:34:39 +0900

    インターフェイスを元にトレーニング記録テーブルで使用する型を定義

--- frontend/app/type/training_record.ts
+++ frontend/app/type/training_record.ts
@@ -4,3 +4,11 @@ export interface BaseTrainingRecord {
   weight: number;
   count: number;
 }
+
+export type TrainingRecordWithName = BaseTrainingRecord & {
+  name: string;
+};
+
+export type TrainingRecordWithExerciseId = BaseTrainingRecord & {
+  exercise_id: number;
+};
