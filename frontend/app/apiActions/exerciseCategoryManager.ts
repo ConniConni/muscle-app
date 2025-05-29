db98431a9e8d71c4f89c75736b2900c72bd9ccaa
ConniConni <nmtgktmo@gmail.com>
2025-05-29 19:08:51 +0900

    マスタ追加APIを別ファイルに切り出し

--- frontend/app/apiActions/exerciseCategoryManager.ts
+++ frontend/app/apiActions/exerciseCategoryManager.ts
@@ -18,3 +18,37 @@ export const getExerciseCategory = async () => {
     alert(`マスタ一覧の取得に失敗しました。\n\n${error.message}`);
   }
 };
+
+export const createNewTraining = async (params: {
+  target_id: number;
+  name: string;
+}) => {
+  console.log(params.name);
+  if (params.target_id > 0 && params.name.length > 0) {
+    try {
+      const response = await fetch(`${API_BASE_URL}/exercise-category`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(params),
+      });
+      console.log("params: ", params);
+      if (response.status != 201) {
+        const errorData = await response.json();
+        throw new Error(
+          `HTTP ${errorData.statusCode} エラー\n${errorData.message}`
+        );
+      }
+      return { success: true };
+    } catch (error: any) {
+      return { success: false, error: error.message };
+    }
+  } else {
+    const alertMessage: string[] = [];
+    if (!params.target_id) alertMessage.push("部位を選択してください");
+    if (!params.name)
+      alertMessage.push("入力画面には1文字以上の文字を入力してください");
+    return { success: false, error: alertMessage.join("\n") };
+  }
+};
--- frontend/app/pages/ExerciseCategoryManagerPage.tsx
+++ frontend/app/pages/ExerciseCategoryManagerPage.tsx
@@ -2,13 +2,14 @@ import { useEffect, useState } from "react";
 import Button from "~/components/parts/Button";
 import Header from "~/components/common/Header";
 import Sidebar from "~/components/common/Sidebar";
-import { API_BASE_URL } from "~/config";
-import { getExerciseCategory } from "~/apiActions/exerciseCategoryManager";
+import {
+  createNewTraining,
+  getExerciseCategory,
+} from "~/apiActions/exerciseCategoryManager";
 import TargetSelectionPulldown from "~/components/parts/pulldown/TargetSelectionPulldown";
 import type { ExerciseCategory } from "~/type/exercise_category";
 
 // 筋トレ種目（マスタ）登録画面を生成する関数コンポーネント
-
 const ExerciseCategoryManagerPage = () => {
   const [exerciseCategory, setExerciseCategory] = useState<ExerciseCategory[]>(
     []
@@ -23,42 +24,20 @@ const ExerciseCategoryManagerPage = () => {
       setExerciseCategory(result);
     })();
   }, []);
+  // マスタ登録APIを呼び出す
+  const handleCreateNewTraining = async () => {
+    const result = await createNewTraining({
+      target_id: selectedTargetId,
+      name: newExerciseCategory,
+    });
 
-  const createNewTraining = async () => {
-    console.log(newExerciseCategory);
-    if (newExerciseCategory.length > 0) {
-      try {
-        const response = await fetch(`${API_BASE_URL}/exercise-category`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            target_id: selectedTargetId,
-            name: newExerciseCategory,
-          }),
-        });
-        if (response.status != 201) {
-          const errorData = await response.json();
-          throw new Error(
-            `HTTP ${errorData.statusCode} エラー\n${errorData.message}`
-          );
-        }
-        alert("マスタへの追加が完了しました。");
-        setNewExerciseCategory("");
-        getExerciseCategory();
-      } catch (error: any) {
-        alert(`マスタ追加に失敗しました。\n\n${error.message}`);
-      }
+    if (result.success) {
+      alert("マスタへの追加が完了しました。");
+      setSelectedTargetId(0);
+      setNewExerciseCategory("");
+      setExerciseCategory(await getExerciseCategory());
     } else {
-      const alertMessage: string[] = [];
-      if (!selectedTargetId) {
-        alertMessage.push("部位を選択してください");
-      }
-      if (!newExerciseCategory) {
-        alertMessage.push("入力画面には1文字以上の文字を入力してください");
-      }
-      alert(alertMessage.join("\n"));
+      alert(`マスタ追加に失敗しました。\n\n${result.error}`);
     }
   };
 
@@ -115,7 +94,7 @@ const ExerciseCategoryManagerPage = () => {
               value={newExerciseCategory}
               onChange={(e) => setNewExerciseCategory(e.target.value)}
             />
-            <Button onClick={createNewTraining} buttonName="マスタ追加" />
+            <Button onClick={handleCreateNewTraining} buttonName="マスタ追加" />
           </div>
           <table>
             <thead>
