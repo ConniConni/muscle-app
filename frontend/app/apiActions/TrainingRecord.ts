cc27749bfdb5398fe3848a5a28158db96cf3e842
ConniConni <nmtgktmo@gmail.com>
2025-05-29 16:15:54 +0900

    TrainingRecord.tsにトレーニング記録登録apiを切り出し

---
+++ frontend/app/apiActions/TrainingRecord.ts
@@ -0,0 +1,38 @@
+import { API_BASE_URL } from "~/config";
+
+// API呼び出し専用の関数
+export const createTrainingRecord = async (params: {
+  exercise_id: number;
+  date: string;
+  weight: number;
+  count: number;
+}) => {
+  if (params.exercise_id && params.date && params.count) {
+    try {
+      const response = await fetch(`${API_BASE_URL}/training-record/`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(params),
+      });
+      if (response.status !== 201) {
+        const errorData = await response.json();
+        throw new Error(
+          `HTTP ${errorData.statusCode} エラー\n${errorData.message}`
+        );
+      }
+      return { success: true };
+    } catch (error: any) {
+      return { success: false, error: error.message };
+    }
+  } else {
+    const alertMessage: string[] = [];
+    if (!params.exercise_id)
+      alertMessage.push("トレーニングを選択してください");
+    if (!params.date) alertMessage.push("実施日を選択してください");
+    if (!params.weight) alertMessage.push("重量を入力してください");
+    if (!params.count) alertMessage.push("回数を入力してください");
+    return { success: false, error: alertMessage.join("\n") };
+  }
+};
