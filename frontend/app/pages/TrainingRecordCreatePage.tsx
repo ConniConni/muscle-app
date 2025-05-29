a7756b6229b8803cb21752a99ab86062db9b5784
ConniConni <nmtgktmo@gmail.com>
2025-05-29 16:17:27 +0900

    トレーニング記録登録はTrainingRecord.tsx内のapiを呼び出す形に変更

--- frontend/app/pages/TrainingRecordCreatePage.tsx
+++ frontend/app/pages/TrainingRecordCreatePage.tsx
@@ -5,9 +5,16 @@ import Sidebar from "~/components/common/Sidebar";
 import { API_BASE_URL } from "~/config";
 import { useEffect, useState } from "react";
 import type { TrainingRecordWithExerciseId } from "~/type/training_record";
+import { createTrainingRecord } from "~/apiActions/TrainingRecord";
 
 // 筋トレ実績登録画面を生成する関数コンポーネント
 const TrainingRecordCreatePage = () => {
+  // トップ画面に戻るための処理
+  const navigate = useNavigate();
+  const backTopPage = () => {
+    navigate("/");
+  };
+
   const [filterVal, setFilterVal] = useState<number>(0);
   // 部位選択プルダウン用のstateを追加
   const [filterTarget, setFilterTarget] = useState<number>(0);
@@ -27,58 +34,27 @@ const TrainingRecordCreatePage = () => {
     console.log("stateの値:", trainingRecord);
   }, [trainingRecord]);
 
-  const createTrainingRecord = async (formData: FormData) => {
-    const categoryId = formData.get("exercise_id");
+  // トレーニング記録登録に必要なデータの取得
+  const handleCreate = async (formData: FormData) => {
+    const exercise_id = formData.get("exercise_id");
     const date = formData.get("date");
     const weight = formData.get("weight");
     const count = formData.get("count");
-    // 筋トレ実績登録処理呼び出し
-    if (categoryId && date && count) {
-      try {
-        const response = await fetch(`${API_BASE_URL}/training-record/`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            exercise_id: +categoryId!,
-            date: date,
-            weight: +weight!,
-            count: +count!,
-          }),
-        });
-        if (response.status != 201) {
-          const errorData = await response.json();
-          throw new Error(
-            `HTTP ${errorData.statusCode} エラー\n${errorData.message} `
-          );
-        }
-        alert("登録が完了しました。");
-        backTopPage();
-      } catch (error: any) {
-        alert(`データの登録に失敗しました。\n\n${error.message}`);
-      }
-    } else {
-      const alertMessage: string[] = [];
-      if (!categoryId) {
-        alertMessage.push("トレーニングを選択してください");
-      }
-      if (!date) {
-        alertMessage.push("実施日を選択してください");
-      }
-      if (!weight) {
-        alertMessage.push("重量を入力してください");
-      }
-      if (!count) {
-        alertMessage.push("回数を入力してください");
-      }
-      alert(alertMessage.join("\n"));
-    }
-  };
 
-  const navigate = useNavigate();
-  const backTopPage = () => {
-    navigate("/");
+    // トレーニング記録登録処理呼び出し
+    const result = await createTrainingRecord({
+      exercise_id: +exercise_id!,
+      date: date as string,
+      weight: +weight!,
+      count: +count!,
+    });
+
+    if (result.success) {
+      alert("登録が完了しました。");
+      backTopPage();
+    } else {
+      alert(`データの登録に失敗しました。\n\n${result.error}`);
+    }
   };
 
   return (
@@ -94,7 +70,7 @@ const TrainingRecordCreatePage = () => {
             setFilterTarget={setFilterTarget}
             trainingRecord={trainingRecord}
             setTrainingRecord={setTrainingRecord}
-            onClick={createTrainingRecord}
+            onClick={handleCreate}
             actionName="登録"
             color="white"
             background="seagreen"
