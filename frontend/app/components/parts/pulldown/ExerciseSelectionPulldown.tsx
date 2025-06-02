8df3810166058ba38359a0473200df23c5bfe1a3
ConniConni <nmtgktmo@gmail.com>
2025-06-02 16:45:23 +0900

    SelectionPulldownコンポーネントのデータ取得はページ呼び出し側で行うように修正

--- frontend/app/components/parts/pulldown/ExerciseSelectionPulldown.tsx
+++ frontend/app/components/parts/pulldown/ExerciseSelectionPulldown.tsx
@@ -1,69 +1,25 @@
-import React, { useEffect, useState } from "react";
-import type { TrainingRecordWithExerciseId } from "~/type/training_record";
+import React from "react";
 import BaseSelectionPulldown from "./BaseSelectionPulldown";
-import { API_BASE_URL } from "~/config";
 import type { PulldownSelectedValue } from "~/type/common";
 
-type ExerciseSelectionPulldownProps = {
-  filterTarget?: number;
-  filterExercise: number;
-  setFilterExercise: React.Dispatch<React.SetStateAction<number>>;
-  trainingRecord?: TrainingRecordWithExerciseId;
-  setTrainingRecord?: React.Dispatch<
-    React.SetStateAction<TrainingRecordWithExerciseId>
-  >;
+type TargetSelectionPulldownProps = {
+  options: PulldownSelectedValue[];
+  value: number;
+  handleValueChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 };
 
 // 種目選択のプルダウンを生成する関数コンポーネント
-const ExerciseSelectionPulldown = ({
-  filterExercise,
-  filterTarget,
-  setFilterExercise,
-  trainingRecord,
-  setTrainingRecord,
-}: ExerciseSelectionPulldownProps) => {
-  // 種目名を取得
-  const [selectedValues, setSelectedValues] = useState<PulldownSelectedValue[]>(
-    []
-  );
-  const apiEndPoint =
-    filterTarget && filterTarget > 0
-      ? `exercise-category/target/${filterTarget}`
-      : "exercise-category";
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(`${API_BASE_URL}/${apiEndPoint}`);
-      const result = await response.json();
-      setSelectedValues(result);
-      console.log("エンドポイント: ", apiEndPoint, ": 取得結果", result);
-    })();
-  }, [apiEndPoint]);
-  // 種目の変更状態を管理するハンドラー
-  const handleExerciseIdChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const newExerciseIdStr = e.target.value;
-    const newExerciseId = newExerciseIdStr === "" ? 0 : +newExerciseIdStr;
-
-    // 登録、更新画面でのプルダウン表示
-    if (trainingRecord !== undefined && setTrainingRecord !== undefined) {
-      setTrainingRecord!({
-        ...trainingRecord!,
-        exercise_id: newExerciseId,
-      });
-      // 筋トレ実績画面でのプルダウン表示
-    } else {
-      setFilterExercise(newExerciseId);
-      console.log(
-        "エンドポイント: ",
-        `exercise-category/target/${filterTarget}`
-      );
-    }
-  };
+const TargetSelectionPulldown = ({
+  options,
+  value,
+  handleValueChange,
+}: TargetSelectionPulldownProps) => {
   return (
     <BaseSelectionPulldown
-      filterVal={filterExercise}
-      handleValueChange={handleExerciseIdChange}
-      selectedValues={selectedValues}
+      filterVal={value}
+      handleValueChange={handleValueChange}
+      selectedValues={options || []}
     />
   );
 };
-export default ExerciseSelectionPulldown;
+export default TargetSelectionPulldown;
--- frontend/app/components/parts/pulldown/TargetSelectionPulldown.tsx
+++ frontend/app/components/parts/pulldown/TargetSelectionPulldown.tsx
@@ -1,61 +1,24 @@
-import React, { useEffect, useState } from "react";
-import type { TrainingRecordWithExerciseId } from "~/type/training_record";
+import React from "react";
 import BaseSelectionPulldown from "./BaseSelectionPulldown";
 import type { PulldownSelectedValue } from "~/type/common";
-import { API_BASE_URL } from "~/config";
 
 type TargetSelectionPulldownProps = {
-  filterTarget: number;
-  setFilterTarget: React.Dispatch<React.SetStateAction<number>>;
-  trainingRecord?: TrainingRecordWithExerciseId;
-  setTrainingRecord?: React.Dispatch<
-    React.SetStateAction<TrainingRecordWithExerciseId>
-  >;
+  options: PulldownSelectedValue[];
+  value: number;
+  handleValueChange: (e: React.ChangeEvent<HTMLSelectElement>) => void;
 };
 
-// 種目選択のプルダウンを生成する関数コンポーネント
+// 部位選択のプルダウンを生成する関数コンポーネント
 const TargetSelectionPulldown = ({
-  filterTarget,
-  setFilterTarget,
-  trainingRecord,
-  setTrainingRecord,
+  options,
+  value,
+  handleValueChange,
 }: TargetSelectionPulldownProps) => {
-  // 部位名を取得
-  const [selectedValues, setSelectedValues] = useState<PulldownSelectedValue[]>(
-    []
-  );
-  const apiEndPoint = "target-area";
-  useEffect(() => {
-    (async () => {
-      const response = await fetch(`${API_BASE_URL}/${apiEndPoint}`);
-      const result = await response.json();
-      setSelectedValues(result);
-      console.log("エンドポイント: ", apiEndPoint, ": 取得結果", result);
-    })();
-  }, [apiEndPoint]);
-
-  // 種目の変更状態を管理するハンドラー
-  const handleTargetIdChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const newTargetIdStr = e.target.value;
-    const newTargetId = newTargetIdStr === "" ? 0 : +newTargetIdStr;
-
-    // 登録、更新画面でのプルダウン表示
-    if (trainingRecord !== undefined && setTrainingRecord !== undefined) {
-      setTrainingRecord!({
-        ...trainingRecord!,
-        target_id: newTargetId,
-      });
-      // 筋トレ実績画面でのプルダウン表示
-    } else {
-      setFilterTarget(newTargetId);
-      console.log("filterTarget: ", filterTarget);
-    }
-  };
   return (
     <BaseSelectionPulldown
-      filterVal={filterTarget}
-      handleValueChange={handleTargetIdChange}
-      selectedValues={selectedValues}
+      filterVal={value}
+      handleValueChange={handleValueChange}
+      selectedValues={options || []}
     />
   );
 };
