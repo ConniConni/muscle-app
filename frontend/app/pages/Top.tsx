c827a175445d23c5be86b9863aa1c937afaa3806
ConniConni <nmtgktmo@gmail.com>
2025-06-05 15:29:31 +0900

    カレンダーを日曜始まりに調整

--- frontend/app/pages/Top.tsx
+++ frontend/app/pages/Top.tsx
@@ -6,14 +6,19 @@ import Sidebar from "~/components/common/Sidebar";
 
 // トップページを生成する関数コンポーネント
 export function Top() {
-  const [date, setDate] = useState<Date | null>(new Date());
+  const [date, setDate] = useState<Date | null>(new Date()); // 初期値は今日の日付
   return (
     <div className="layout">
       <Header />
       <div className="main-content">
         <Sidebar />
         <div className="content">
-          <Calendar value={date} onClickDay={(e) => setDate(e)} />
+          <Calendar
+            value={date}
+            onClickDay={(e) => setDate(e)} // 選択した日にポインタを当てる
+            calendarType="gregory" // 日曜始り、土日休日とするためにグレゴリオ歴を指定
+            locale="en-US" // 日付の「日」の表示を消すために、英語ロケールを使用
+          />
         </div>
       </div>
     </div>
