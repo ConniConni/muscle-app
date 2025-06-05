9a5dbd4b4058b51d1141167d09d033ecce5a5dd9
ConniConni <nmtgktmo@gmail.com>
2025-06-05 15:00:11 +0900

    カレンダーを表示するためのライブラリをreact-calendarに変更

--- frontend/app/pages/Top.tsx
+++ frontend/app/pages/Top.tsx
@@ -1,22 +1,19 @@
-import { Calendar } from "primereact/Calendar";
 import { useState } from "react";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 import Header from "~/components/common/Header";
 import Sidebar from "~/components/common/Sidebar";
 
 // トップページを生成する関数コンポーネント
 export function Top() {
-  const [date, setDate] = useState<any>();
-  // カスタムロケール設定
-  const customLocale = {
-    dayNamesShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
-  };
+  const [date, setDate] = useState<Date | null>(new Date());
   return (
     <div className="layout">
       <Header />
       <div className="main-content">
         <Sidebar />
         <div className="content">
-          <Calendar value={date} onChange={(e) => setDate(e.value)} inline />
+          <Calendar value={date} onClickDay={(e) => setDate(e)} />
         </div>
       </div>
     </div>
