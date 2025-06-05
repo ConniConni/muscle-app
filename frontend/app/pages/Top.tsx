2e55393e38e1bf35bde1c152f14aa3849947dffb
ConniConni <nmtgktmo@gmail.com>
2025-06-05 12:12:59 +0900

    トップ画面に基本のカレンダーを表示

--- frontend/app/pages/Top.tsx
+++ frontend/app/pages/Top.tsx
@@ -1,15 +1,22 @@
+import { Calendar } from "primereact/Calendar";
+import { useState } from "react";
 import Header from "~/components/common/Header";
 import Sidebar from "~/components/common/Sidebar";
 
 // トップページを生成する関数コンポーネント
 export function Top() {
+  const [date, setDate] = useState<any>();
+  // カスタムロケール設定
+  const customLocale = {
+    dayNamesShort: ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"],
+  };
   return (
     <div className="layout">
       <Header />
       <div className="main-content">
         <Sidebar />
         <div className="content">
-          <h1>トップページ</h1>
+          <Calendar value={date} onChange={(e) => setDate(e.value)} inline />
         </div>
       </div>
     </div>
