950954c574d3382ed29730c94676df8bf845f9f5
ConniConni <nmtgktmo@gmail.com>
2025-06-06 12:12:20 +0900

    日付選択時、トレーニング実績の有無で遷移先が変わるように修正

--- frontend/app/pages/Top.tsx
+++ frontend/app/pages/Top.tsx
@@ -3,13 +3,18 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import { useNavigate } from "react-router";
 import { getHolidayList } from "~/apiActions/holidaysApi";
+import { getTrainingRecord } from "~/apiActions/TrainingRecord";
 import Header from "~/components/common/Header";
 import Sidebar from "~/components/common/Sidebar";
+import type { TrainingRecordWithName } from "~/type/training_record";
 
 // トップページを生成する関数コンポーネント
 export function Top() {
   const [date, setDate] = useState<Date | null>(new Date()); // 初期値は今日の日付
   const [holidays, setHolidays] = useState<Record<string, string>>({});
+  const [trainingRecords, setTrainingRecords] = useState<
+    TrainingRecordWithName[]
+  >([]);
   const navigate = useNavigate();
 
   // APIから祝日データを取得
@@ -35,6 +40,16 @@ export function Top() {
     return dateStr in holidays;
   };
 
+  // 筋トレ実績一覧を取得
+  useEffect(() => {
+    (async () => {
+      const result = await getTrainingRecord();
+      if (result.success) {
+        setTrainingRecords(result.data);
+      }
+    })();
+  }, []);
+
   // クリック時に日付を「yyyy-mm-dd」の形式で取得取得
   const formatDate = (date: Date): string => {
     const year = date.getFullYear();
@@ -48,6 +63,23 @@ export function Top() {
     if (view !== "month") return "";
     return isHoliday(date) ? "holiday" : "";
   };
+
+  // 日付クリック時の処理
+  const handleClickDay = (e: Date) => {
+    setDate(e); // 選択した日をハイライト
+    const formattedDate = formatDate(e);
+    const isTrainingRecord = trainingRecords.some(
+      (trainingRecord) =>
+        (trainingRecord.date as unknown as string).split("T")[0] ===
+        formattedDate
+    ); // 画面の挙動に問題はなかったが、rainingRecord.dateがDate型であることが原因でsplitでエラーになったためas unknown as stringを追加
+    if (isTrainingRecord) {
+      navigate(`list/${formattedDate}`);
+    } else {
+      navigate("/create");
+    }
+  };
+
   return (
     <div className="layout">
       <Header />
@@ -56,11 +88,7 @@ export function Top() {
         <div className="content">
           <Calendar
             value={date}
-            onClickDay={(e) => {
-              setDate(e); // 選択した日をハイライト
-              const formattedDate = formatDate(e);
-              navigate(`list/${formattedDate}`);
-            }}
+            onClickDay={handleClickDay}
             calendarType="gregory" // 日曜始り、土日休日とするためにグレゴリオ歴を指定
             locale="en-US" // 日付の「日」の表示を消すために、英語ロケールを使用
             tileClassName={tileClassName}
