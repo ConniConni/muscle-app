11078b0cd5d61db5e44337d1b34baa96476e5145
ConniConni <nmtgktmo@gmail.com>
2025-06-13 17:44:52 +0900

    ユーザー登録apiの引数のバリデーションを追加

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,1 +1,29 @@
-export class CreateUserDto {}
+import {
+  IsEmail,
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  username: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  userId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @MinLength(8) // パスワードは8文字以上
+  @MaxLength(64) // 64文字以下
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z0-9]+$/) //英大文字・英小文字・数字をすべて1文字以上含み、英数字のみであること
+  password: string;
+}
