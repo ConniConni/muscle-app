8835473499e07b9657ab772b82c8b5b04a144933
ConniConni <nmtgktmo@gmail.com>
2025-06-13 19:00:24 +0900

    ユーザー更新apiを追加

--- backend/src/user/user.service.ts
+++ backend/src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { PrismaService } from 'src/prisma.service';
 import { formatInTimeZone } from 'date-fns-tz';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UserService {
@@ -16,16 +17,24 @@ export class UserService {
     return createUser;
   }
 
+  async update(id: number, updateUserDto: UpdateUserDto) {
+    const updateUser = await this.prisma.user.update({
+      where: {
+        id: id,
+      },
+      data: {
+        ...updateUserDto,
+      },
+    });
+    return updateUser;
+  }
+
   // findAll() {
   //   return `This action returns all user`;
   // }
 
   // findOne(id: number) {
   //   return `This action returns a #${id} user`;
-  // }
-
-  // update(id: number, updateUserDto: UpdateUserDto) {
-  //   return `This action updates a #${id} user`;
   // }
 
   // remove(id: number) {
