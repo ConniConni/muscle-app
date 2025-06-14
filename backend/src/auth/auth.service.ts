60918d2653b8d94d9370c9081b2f36f33e53333f
ConniConni <nmtgktmo@gmail.com>
2025-06-14 12:30:24 +0900

    サインインapi追加

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -1,4 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private userService: UserService) {}
+
+  async signIn(signInDto: SignInDto): Promise<any> {
+    const { userId, password: inputPassword } = signInDto;
+    const user = await this.userService.findByUserId(userId);
+    if (user?.password !== inputPassword) {
+      throw new UnauthorizedException();
+    }
+    // オブジェクトの分割代入で、userからpasswordを除いた残りの情報をresultに格納
+    const { password, ...result } = user;
+    // TODO: JWTを生成して返却する形に修正 現在はユーザー情報をオブジェクトで返却
+    return result;
+  }
+}
