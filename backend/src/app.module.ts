94908aff120626cad2396d25610ec6f721572c73
ConniConni <nmtgktmo@gmail.com>
2025-06-14 09:37:20 +0900

    authモジュールを追加

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -6,9 +6,10 @@ import { PrismaService } from './prisma.service';
 import { ExerciseCategoryModule } from './exercise-category/exercise-category.module';
 import { TargetAreaModule } from './target-area/target-area.module';
 import { UserModule } from './user/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TrainingRecordModule, ExerciseCategoryModule, TargetAreaModule, UserModule],
+  imports: [TrainingRecordModule, ExerciseCategoryModule, TargetAreaModule, UserModule, AuthModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
 })
