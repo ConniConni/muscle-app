1b1447bc50f15694f72bfb8004ea6630758396fa
ConniConni <nmtgktmo@gmail.com>
2025-06-19 14:17:33 +0900

    PrivteRouter.tsxの中身を整理

--- frontend/app/auth/PrivateRoutes.tsx
+++ frontend/app/auth/PrivateRoutes.tsx
@@ -1,20 +1,18 @@
-import React from "react";
 import { Navigate, Outlet } from "react-router-dom";
-import { useAuth } from "./AuthContext"; // AuthContextのパスは適宜調整
+import { useAuth } from "./AuthContext";
 
 const PrivateRoutes = () => {
   const { user, loading } = useAuth();
 
   // 初回読み込み中の場合（認証状態を確認中）
+  // Loadingを表示
   if (loading) {
-    // ローディングスピナーなどを表示するのが親切
-    // ここでは何も表示しないか、ローディング画面コンポーネントを返す
-    return <div>Loading...</div>; // または null
+    return <div>Loading...</div>;
   }
 
   // 読み込みが完了し、ユーザー情報が存在する場合 (ログイン済み)
+  // 子ルート（保護されたページ）を表示
   if (user) {
-    // 子ルート（保護されたページ）を表示
     return <Outlet />;
   }
 
