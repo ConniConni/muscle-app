18eb8240b9b7b79d787216b07ac43b885e01270e
ConniConni <nmtgktmo@gmail.com>
2025-06-20 18:52:31 +0900

    ユーザー登録画面にフォームを追加

--- frontend/app/pages/SignUpPage.tsx
+++ frontend/app/pages/SignUpPage.tsx
@@ -1,4 +1,27 @@
+import { useState } from "react";
+import Header from "~/components/common/Header";
+import SignUpForm from "~/components/parts/SignUpForm";
+import type { SignUpFormType } from "~/type/signup";
+
 const SignupPage = () => {
-  return <h1>ユーザー登録ページ</h1>;
+  const [formData, setFormData] = useState<SignUpFormType>({
+    userId: "",
+    password: "",
+    confirmPassword: "",
+    email: "",
+    confirmEmail: "",
+    username: "",
+  });
+  return (
+    <div className="layout">
+      <Header />
+      <div className="main-content">
+        <div className="content">
+          <h1 className="page-title">ユーザー登録</h1>
+          <SignUpForm {...formData} setFormData={setFormData} />
+        </div>
+      </div>
+    </div>
+  );
 };
 export default SignupPage;
