f0a8eed382cebd02f23a655ecef0685f179ffb51
ConniConni <nmtgktmo@gmail.com>
2025-06-21 23:12:59 +0900

    サインアップページコンポーネントでユーザー登録api呼び出し処理を追加

--- frontend/app/pages/SignUpPage.tsx
+++ frontend/app/pages/SignUpPage.tsx
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useNavigate } from "react-router";
+import { postUser } from "~/apiActions/signUpApi";
 import Header from "~/components/common/Header";
 import SignUpForm from "~/components/parts/SignUpForm";
 import type { SignUpFormType } from "~/type/signup";
@@ -12,13 +14,32 @@ const SignupPage = () => {
     confirmEmail: "",
     username: "",
   });
+  const navigate = useNavigate();
+  // ★ フォーム送信時に実行されるハンドラ関数を定義
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    // フォーム送信によるページのリロードを防ぐ
+    e.preventDefault();
+
+    const response = await postUser(formData);
+
+    if (response.success) {
+      alert("ユーザー登録が完了しました。ログインページに移動します。");
+      navigate("/login");
+    } else {
+      alert(`登録に失敗しました。\n\n${response.error}`);
+    }
+  };
   return (
     <div className="layout">
       <Header />
       <div className="main-content">
         <div className="content">
           <h1 className="page-title">ユーザー登録</h1>
-          <SignUpForm {...formData} setFormData={setFormData} />
+          <SignUpForm
+            {...formData}
+            setFormData={setFormData}
+            onSubmit={handleSubmit}
+          />
         </div>
       </div>
     </div>
