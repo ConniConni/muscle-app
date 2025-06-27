68bf36254a6d00144a3b4b4088195371998331b9
ConniConni <nmtgktmo@gmail.com>
2025-06-27 15:59:25 +0900

    disabledを取得できるようにButtonコンポーネントを修正

--- frontend/app/components/parts/Button.tsx
+++ frontend/app/components/parts/Button.tsx
@@ -14,22 +14,38 @@ type Props = {
 // ボタンを生成する関数コンポーネント
 const Button = (prop: Props) => {
   const [isHovered, setIsHovered] = useState<Boolean>(false);
+  // disabled時のスタイルを定義
+  const disabledStyle = {
+    backgroundColor: "#ccc", // 背景を薄いグレーに
+    color: "#666", // 文字色を濃いグレーに
+    cursor: "not-allowed", // カーソルを「禁止」マークに
+    border: "solid 1px #ccc",
+  };
+
+  // 通常時のスタイルを定義
+  const enabledStyle = {
+    color: isHovered ? prop.hoverColor || "white" : prop.color || "black",
+    background: isHovered
+      ? prop.hoverBackground || "darkgray"
+      : prop.background || "lightgrey",
+    border: `solid 1px ${prop.background || "lightgrey"}`,
+    cursor: "pointer",
+  };
   return (
     <button
       onClick={prop.onClick}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
       type={prop.type || "button"}
+      disabled={prop.disabled}
       style={{
-        color: isHovered ? prop.hoverColor || "white" : prop.color || "black",
-        background: isHovered
-          ? prop.hoverBackground || "darkgray"
-          : prop.background || "lightgrey",
-        border: `solid 1px ${prop.background || "lightgrey"}`,
+        // 共通スタイル
         borderRadius: "10px",
         maxWidth: "250px",
         padding: "5px",
         margin: "5px",
+        // disabledの状態に応じてスタイルを切り替える
+        ...(prop.disabled ? disabledStyle : enabledStyle),
       }}
     >
       {prop.buttonName}
