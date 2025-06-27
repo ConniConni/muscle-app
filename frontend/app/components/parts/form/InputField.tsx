c452542ba104249047a3f82088a7d80beddc8c25
ConniConni <nmtgktmo@gmail.com>
2025-06-27 16:01:15 +0900

    InputFieldがclassNameを受け取れるように修正

--- frontend/app/components/parts/form/InputField.tsx
+++ frontend/app/components/parts/form/InputField.tsx
@@ -6,17 +6,21 @@ type InputFieldProps = {
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   id?: string;
   placeholder?: string;
+  className?: string;
 };
 
 // labelとidはinput要素の中身以外で使用するため明示的にpropsを受け取る
-const InputField = ({ label, id, ...inputProps }: InputFieldProps) => {
+const InputField = ({
+  label,
+  id,
+  className,
+  ...inputProps
+}: InputFieldProps) => {
   const inputId = id || inputProps.name;
   return (
-    <div>
+    <div className={className}>
       <label htmlFor={inputId}>{label}</label>
-      <div>
-        <input id={inputId} {...inputProps} />
-      </div>
+      <input id={inputId} {...inputProps} />
     </div>
   );
 };
