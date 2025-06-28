de8f486713d145687c19cb2243b5c5bf94f85f50
ConniConni <nmtgktmo@gmail.com>
2025-06-28 12:21:46 +0900

    InputFieldPropsを他のコンポーネントも使えるように修正

--- frontend/app/components/parts/form/InputField.tsx
+++ frontend/app/components/parts/form/InputField.tsx
@@ -1,14 +1,4 @@
-type InputFieldProps = {
-  label: React.ReactNode;
-  name: string;
-  type: "text" | "password" | "email";
-  value: string;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  id?: string;
-  placeholder?: string;
-  className?: string;
-  disabled?: boolean;
-};
+import type { InputFieldProps } from "~/type/common";
 
 // labelとidはinput要素の中身以外で使用するため明示的にpropsを受け取る
 const InputField = ({
