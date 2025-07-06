9569b16a5f5288e28cd8c4ecb69801d65983b8f5
ConniConni <nmtgktmo@gmail.com>
2025-07-06 15:32:07 +0900

    getTrainingRecode()関数でHTTPリクエストを行う際、ヘルパー関数を利用し認証ヘッターを取得する

--- frontend/app/apiActions/TrainingRecord.ts
+++ frontend/app/apiActions/TrainingRecord.ts
@@ -1,13 +1,11 @@
 import { API_BASE_URL } from "~/config";
+import { getAuthHeaders } from "./apiHelper";
 
 // 筋トレ実績一覧取得処理呼び出し
-export const getTrainingRecord = async (
-  token: string,
-  options?: {
-    exercise_id?: any;
-    date?: string;
-  }
-) => {
+export const getTrainingRecord = async (options?: {
+  exercise_id?: any;
+  date?: string;
+}) => {
   const params = new URLSearchParams();
   // exercise_idが1以上のときのみパラメータに追加
   if (options?.exercise_id && options.exercise_id > 0)
@@ -19,7 +17,8 @@ export const getTrainingRecord = async (
   }`;
   try {
     const response = await fetch(url, {
-      headers: { Authorization: `Bearer ${token}` },
+      method: "GET",
+      headers: getAuthHeaders(),
     });
     if (response.status !== 200) {
       const errorData = await response.json();
