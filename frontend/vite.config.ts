aeedfb69a4e4f30213d6e52226ac2d707e981104
ConniConni <nmtgktmo@gmail.com>
2025-07-21 12:22:14 +0900

    docker compose biludが実行可能になるように修正

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,22 @@
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { API_BASE_URL } from "~/config";
 
 export default defineConfig({
   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  resolve: {
+    alias: {
+      // '~' というエイリアスを、'./app' ディレクトリへの絶対パスとして設定する
+      "~": path.resolve(__dirname, "./app"),
+    },
+  },
   server: {
     proxy: {
-      // '/api' で始まるリクエストを http://localhost:3000 に転送
+      // '/api' で始まるリクエストをAPI_BASE_URLに転送
       "/api": {
-        target: API_BASE_URL,
+        target: "http://localhost:3000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
