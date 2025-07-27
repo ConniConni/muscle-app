35a23e0a289c5964131392d27d594d7b4866e342
ConniConni <nmtgktmo@gmail.com>
2025-07-27 17:09:07 +0900

    friendshipエンティティのうちフレンド申請APIに不要な部分を削除 #64

--- backend/src/friendship/friendship.controller.ts
+++ backend/src/friendship/friendship.controller.ts
@@ -1,7 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { FriendshipService } from './friendship.service';
 import { CreateFriendshipDto } from './dto/create-friendship.dto';
-import { UpdateFriendshipDto } from './dto/update-friendship.dto';
 
 @Controller('friendship')
 export class FriendshipController {
@@ -10,25 +17,5 @@ export class FriendshipController {
   @Post()
   create(@Body() createFriendshipDto: CreateFriendshipDto) {
     return this.friendshipService.create(createFriendshipDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.friendshipService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.friendshipService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateFriendshipDto: UpdateFriendshipDto) {
-    return this.friendshipService.update(+id, updateFriendshipDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.friendshipService.remove(+id);
   }
 }
