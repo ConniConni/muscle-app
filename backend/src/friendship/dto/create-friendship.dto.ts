2a16bfa0d8b9d360ee0ca2feeea74c200135ac46
ConniConni <nmtgktmo@gmail.com>
2025-07-27 18:08:47 +0900

    feat: フレンド申請apiのdtoを定義 #64

--- backend/src/friendship/dto/create-friendship.dto.ts
+++ backend/src/friendship/dto/create-friendship.dto.ts
@@ -1,1 +1,11 @@
-export class CreateFriendshipDto {}
+import { IsNotEmpty, IsNumber, IsPositive } from 'class-validator';
+
+export class CreateFriendshipDto {
+  @IsNotEmpty()
+  @IsPositive()
+  approvalUserId: number;
+
+  @IsNotEmpty()
+  @IsNumber()
+  status: number;
+}
