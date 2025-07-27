1029510bec143fed2317432f4f67442a8bc056b2
ConniConni <nmtgktmo@gmail.com>
2025-07-27 18:08:37 +0900

    feat: フレンド申請apiのサービスを定義 #64

--- backend/src/friendship/friendship.service.ts
+++ backend/src/friendship/friendship.service.ts
@@ -1,9 +1,46 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  ConflictException,
+  Injectable,
+} from '@nestjs/common';
 import { CreateFriendshipDto } from './dto/create-friendship.dto';
+import { PrismaService } from 'src/prisma.service';
 
 @Injectable()
 export class FriendshipService {
-  create(createFriendshipDto: CreateFriendshipDto) {
-    return 'This action adds a new friendship';
+  constructor(private prisma: PrismaService) {}
+  async create(
+    createFriendshipDto: CreateFriendshipDto,
+    requesterUserId: number,
+  ) {
+    const { approvalUserId } = createFriendshipDto;
+    // 申請先が自分自身の場合は例外処理を発生させる
+    if (requesterUserId === createFriendshipDto.approvalUserId) {
+      throw new BadRequestException(
+        '自分自身にフレンド申請を送ることはできません。',
+      );
+    }
+    // フレンド申請済もしくはフレンド関係にある場合は例外処理を発生させる
+    const existingRequest = await this.prisma.friendship.findFirst({
+      where: {
+        OR: [
+          { requesterUserId: requesterUserId, approvalUserId: approvalUserId },
+          { requesterUserId: approvalUserId, approvalUserId: requesterUserId }, // 逆方向の申請もチェック
+        ],
+      },
+    });
+    if (existingRequest) {
+      throw new ConflictException(
+        '既にフレンド申請が存在するか、フレンド関係にあります。',
+      );
+    }
+    const friendRequest = await this.prisma.friendship.create({
+      data: {
+        requesterUserId: requesterUserId,
+        approvalUserId: approvalUserId,
+        status: 0,
+      },
+    });
+    return friendRequest;
   }
 }
