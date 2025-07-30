430bb7ff2eb78f91a17c100bc1e970a44baca361
ConniConni <nmtgktmo@gmail.com>
2025-07-30 20:57:08 +0900

    fix: これまでのmigrationファイルを削除  #78

--- backend/prisma/migrations/20250330120007_muscle_training/migration.sql
+++
@@ -1,2 +0,0 @@
--- CreateIndex
-CREATE INDEX "muscle_training_category_id_idx" ON "muscle_training"("category_id");
--- backend/prisma/migrations/20250525021002_change_type_weigt_int_to_float/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "training_records" ALTER COLUMN "weight" SET DATA TYPE DOUBLE PRECISION;
