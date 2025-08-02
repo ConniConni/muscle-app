be6f9d509f81e7f6f968e9ad40193ad516822f66
ConniConni <nmtgktmo@gmail.com>
2025-08-02 16:29:23 +0900

    refactor: UpdateFriendship.dtoのマジックナンバーを修正

--- backend/src/friendship/dto/update-friendship.dto.ts
+++ backend/src/friendship/dto/update-friendship.dto.ts
@@ -1,9 +1,10 @@
 import { IsInt, IsNotEmpty, IsIn } from 'class-validator';
+import RequestStatus from 'src/common/requestStatus';
 
 export class UpdateFriendshipDto {
   @IsInt()
   @IsNotEmpty()
-  // statusに指定できる数値を、[1, 2] に限定する
-  @IsIn([1, 2])
+  // statusに指定できる値を、[承認, 拒否] に限定する
+  @IsIn([RequestStatus.ACCEPTED, RequestStatus.DECLINED])
   status: number;
 }
