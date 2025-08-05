9fcd7b939385a7e250596aff167f16ce1a9f0509
ConniConni <nmtgktmo@gmail.com>
2025-08-05 19:28:36 +0900

    feat: フレンド一覧画面の共通部分を追加  #69

--- frontend/app/pages/MyFriendsListPage.tsx
+++ frontend/app/pages/MyFriendsListPage.tsx
@@ -1,5 +1,18 @@
+import Header from "~/components/common/Header";
+import Sidebar from "~/components/common/Sidebar";
+
 const MyFriendsListPage = () => {
-  return <h1>MyFriendsListPage</h1>;
+  return (
+    <div className="layout">
+      <Header />
+      <div className="main-content">
+        <Sidebar />
+        <div className="content">
+          <h1 className="page-title">フレンド一覧</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MyFriendsListPage;
