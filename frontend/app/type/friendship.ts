e9efc601b111912b4513c9ae80dfb65500888bfd
ConniConni <nmtgktmo@gmail.com>
2025-08-17 18:20:51 +0900

    feat:型を追加 #97

--- frontend/app/type/friendship.ts
+++ frontend/app/type/friendship.ts
@@ -1,4 +1,4 @@
-export type Users = {
+export type User = {
   id: number;
   username: string;
 };
@@ -6,4 +6,10 @@ export type Users = {
 export type Friend = {
   id: number;
   username: string;
+};
+
+export type FriendshipStatus = "PENDING" | "ACCEPTED" | null;
+
+export type UserWithFriendshipStatus = User & {
+  friendshipStatus: FriendshipStatus;
 };
