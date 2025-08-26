f70e086929b23d1dc61180168992a082ef02c017
ConniConni <nmtgktmo@gmail.com>
2025-08-26 21:36:19 +0900

    feat:フレンド承認画面にデザインを適用 #99

--- frontend/app/pages/FriendshipAcceptedPage.tsx
+++ frontend/app/pages/FriendshipAcceptedPage.tsx
@@ -1,4 +1,17 @@
+import Header from "~/components/common/Header";
+import Sidebar from "~/components/common/Sidebar";
+
 const FriendshipAcceptedPage = () => {
-  return <p>フレンド承認画面</p>;
+  return (
+    <div className="layout">
+      <Header />
+      <div className="main-content">
+        <Sidebar />
+        <div className="content">
+          <h1 className="page-title">フレンド承認画面</h1>
+        </div>
+      </div>
+    </div>
+  );
 };
 export default FriendshipAcceptedPage;
