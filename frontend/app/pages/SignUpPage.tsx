f13a778ac990ba691c5e792786d2a863e1a37db6
ConniConni <nmtgktmo@gmail.com>
2025-09-11 19:00:11 +0900

    サインアップページのアラートをダイアログに変更

--- frontend/app/pages/SignUpPage.tsx
+++ frontend/app/pages/SignUpPage.tsx
@@ -1,12 +1,20 @@
 import { useState } from "react";
 import { useNavigate } from "react-router";
 import { postUser } from "~/apiActions/SignUpApi";
+import AlertDialog from "~/components/common/AlertDialog";
 import Header from "~/components/common/Header";
 import SignupForm from "~/components/parts/form/SignupForm";
+import type { DialogState } from "~/type/common";
 import type { SignupFormType } from "~/type/signup";
 
 const SignupPage = () => {
   const [isSubmitting, setIsSubmitting] = useState(false);
+  // ダイアログの状態を管理するstate
+  const [dialog, setDialog] = useState<DialogState>({
+    open: false,
+    title: "",
+    message: "",
+  });
   const navigate = useNavigate();
   // キャンセルボタンクリック時に実行されるハンドラ関数を定義
   const handleCancel = () => {
@@ -21,10 +29,28 @@ const SignupPage = () => {
     setIsSubmitting(false); // 送信完了(ボタンを有効に戻す)
 
     if (response.success) {
-      alert("ユーザー登録が完了しました。ログインページに移動します。");
-      navigate("/login");
+      console.log("成功ダイアログ");
+      // 成功ダイアログを表示するstateを更新
+      setDialog({
+        open: true,
+        title: "ユーザー登録完了",
+        message: "ログインページに移動します。",
+      });
     } else {
-      alert(`登録に失敗しました。\n\n${response.error}`);
+      // エラーダイアログを表示するstateを更新
+      setDialog({
+        open: true,
+        title: "ユーザー登録失敗",
+        message: response.error,
+      });
+    }
+  };
+  // ダイアログの外をクリックしたときにダイアログを閉じる処理
+  const handleCloseDialog = async () => {
+    setDialog({ ...dialog, open: false });
+    // 成功ダイアログを閉じた場合、ページ遷移する
+    if (dialog.title === "ユーザー登録完了") {
+      navigate("/login");
     }
   };
   return (
@@ -38,6 +64,7 @@ const SignupPage = () => {
             onCancel={handleCancel}
             isSubmitting={isSubmitting}
           />
+          <AlertDialog dialog={dialog} handleCloseDialog={handleCloseDialog} />
         </div>
       </div>
     </div>
